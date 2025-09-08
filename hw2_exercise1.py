2ff529a5b770ea995b70aa9662ed391af8ac4eb8
asoliz <alansoliz@gmail.com>
2025-09-08 11:49:37 -0700

    updated locator for cod and privacy links

--- hw2_exercise1.py
+++ hw2_exercise1.py
@@ -15,9 +15,7 @@ driver.maximize_window()
 
 # 1. Practice with locators.
 # Create locators + search strategy for these page elements of Amazon Sign in page:
-driver.get('https://www.amazon.com')
-sleep(3)
-driver.find_element(By.ID, "nav-link-accountList").click()
+driver.get('https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0')
 sleep(3)
 
 # Amazon logo, search by Class, "a-link-nav-icon"
@@ -27,9 +25,9 @@ driver.find_element(By.ID, "ap_email")
 # Continue button, search by ID, "continue"
 driver.find_element(By.ID, "continue")
 # Conditions of use link, search by ID and LinkText
-driver.find_element(By.XPATH, "//a[contains(@href, 'ap_signin_notification_condition_of_use')]")
+driver.find_element(By.XPATH, "//div[@id='legalTextRow']//a[contains(@href, 'ap_signin_notification_condition_of_use')]")
 # Privacy Notice link
-driver.find_element(By.XPATH, "//a[contains(@href, 'ap_signin_notification_privacy_notice')]")
+driver.find_element(By.XPATH, "//div[@id='legalTextRow']//a[contains(@href, 'ap_signin_notification_privacy_notice')]")
 # Need help link, search by Class, "a-expander-prompt"
 driver.find_element(By.CLASS_NAME, "a-expander-prompt")
 # Forgot your password link , search by ID, "auth-fpp-link-bottom"
